2e2f2e16e8cd25269f77228f57efc6a67431d968
Timothy LaRock <timothylarock@gmail.com>
2023-04-11 12:37:07 +0100

    Combined plotting functions.

--- src/plot_simulation_results.py
+++ src/plot_simulation_results.py
@@ -35,28 +35,67 @@ def plot_cumulative(configuration, results_obs, results_rnd,
     output_obs and output_rnd are outputs of run_many_simulations function
     in run_simulations.py.
 """
-def plot_cumulative_averages(configuration, output_obs, output_rnd,
+def plot_cumulative_averages(output_obs, output_rnd, num_steps=0,
                     first_label="Observed", second_label="Randomized",
-                             yscale="linear"):
+                    yscale="linear", sizes=False):
 
-    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(7,3), squeeze=False)
+    # Plo setup
+    if num_steps <= 0:
+        num_steps = output_obs["nodes_activated"].shape[1]
+
+    ncols = 2
+    if sizes:
+        ncols = 3
+
+    fig, axs = plt.subplots(nrows=1, ncols=ncols, figsize=(7,3), squeeze=False)
     fig.subplots_adjust(wspace=0.3)
 
-    x = list(range(configuration["steps"]+1))
+    x = list(range(num_steps))
     labels = ["Cumulative Nodes Activated", "Cumulative Edges Activated"]
     for col_idx, key in enumerate(["nodes_activated", "edges_activated"]):
-        mean = np.mean(np.cumsum(output_obs[key], axis=1), axis=0)
-        std = np.std(np.cumsum(output_obs[key], axis=1), axis=0)
+        obs = output_obs[key][:, 0:num_steps]
+        mean = np.mean(np.cumsum(obs, axis=1), axis=0)
+        std = np.std(np.cumsum(obs, axis=1), axis=0)
         axs[0][col_idx].plot(x, mean, label=first_label)
         axs[0][col_idx].fill_between(x, mean-std, mean+std, alpha=0.3)
 
-        mean = np.mean(np.cumsum(output_rnd[key], axis=1), axis=0)
-        std = np.std(np.cumsum(output_rnd[key], axis=1), axis=0)
+        rnd = output_rnd[key][:, 0:num_steps]
+        mean = np.mean(np.cumsum(rnd, axis=1), axis=0)
+        std = np.std(np.cumsum(rnd, axis=1), axis=0)
         axs[0][col_idx].plot(x, mean, label=second_label)
         axs[0][col_idx].fill_between(x, mean-std, mean+std, alpha=0.3)
-        axs[0][col_idx].set(xlabel="Time", ylabel=labels[col_idx],
-                            yscale=yscale)
+        axs[0][col_idx].set(xlabel="Time", ylabel=labels[col_idx], yscale=yscale)
         axs[0][col_idx].legend()
+
+    if sizes:
+        # Plot the average size of the hyperedges
+        # ToDo: I should plot real average as reference
+        col_idx = 2
+        key = "activated_edge_sizes"
+        obs_cmean = np.zeros(output_obs[key].shape)
+        obs_cmean[:, 0] = output_obs[key][:, 0]
+        rnd_cmean = np.zeros(output_obs[key].shape)
+        rnd_cmean[:, 0] = output_rnd[key][:, 0]
+        for t in range(1, obs_cmean.shape[1]):
+            obs_cmean[:, t] = np.nanmean(output_obs[key][:,0:t],
+                                        where=output_obs[key][:,0:t]>0,
+                                        axis=1)
+            rnd_cmean[:, t] = np.nanmean(output_rnd[key][:,0:t],
+                                        where=output_rnd[key][:,0:t]>0,
+                                        axis=1)
+
+        mean = np.mean(obs_cmean, axis=0)
+        std = np.std(obs_cmean, axis=0)
+        axs[0][col_idx].plot(x, mean, label="Observed")
+        axs[0][col_idx].fill_between(x, mean-std, mean+std, alpha=0.3)
+
+        mean = np.mean(rnd_cmean, axis=0)
+        std = np.std(rnd_cmean, axis=0)
+        axs[0][col_idx].plot(x, mean, label="Randomized")
+        axs[0][col_idx].fill_between(x, mean-std, mean+std, alpha=0.3)
+        axs[0][col_idx].set(xlabel="Time", ylabel="Average Size")
+
+
     return fig, axs
 
 
