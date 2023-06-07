3ca3500ea0e9fddd5e1486b30196a6b207716529
Timothy LaRock <timothylarock@gmail.com>
2023-06-07 12:53:33 +0100

    Added compare_randomizations code.

---
+++ src/compare_randomizations.py
@@ -0,0 +1,63 @@
+from encapsulation_dag import encapsulation_dag, overlap_dag, overlap_graph
+from utils import read_data, read_hyperedges, largest_connected_component
+from layer_randomization import layer_randomization
+
+def print_observed_stats(cc):
+    print("Computing encapsulation DAG...")
+    obs_encap, _, _ = encapsulation_dag(cc)
+    print("Number of encapsulation DAG edges in observed data: " + str(obs_encap.number_of_edges()))
+
+    print("Computing overlap DAG...")
+    obs_overlap_dag, _, _ = overlap_dag(cc)
+    print("Number of overlap DAG in observed data: " + str(obs_overlap_dag.number_of_edges()))
+
+    print("Computing overlap graph...")
+    obs_overlap, _, _ = overlap_graph(cc)
+    print("Number of overlap edges in observed data: " + str(obs_overlap.number_of_edges()))
+    return obs_encap.number_of_edges(), obs_overlap_dag.number_of_edges(), obs_overlap.number_of_edges()
+
+
+def print_random_stats(cc, obs_encap, obs_overdag, obs_overlap):
+    print("Computing encapsulation DAG...")
+    rnd_encap, _, _ = encapsulation_dag(cc)
+    print(f"Number of encapsulation DAG edges in random data: {rnd_encap.number_of_edges()} %: {rnd_encap.number_of_edges() / obs_encap}")
+
+    print("Computing overlap DAG...")
+    rnd_overlap_dag, _, _ = overlap_dag(cc)
+    print(f"Number of overlap DAG edges in random data: {rnd_overlap_dag.number_of_edges()} %: {rnd_overlap_dag.number_of_edges() / obs_overdag}")
+
+    print("Computing overlap graph...")
+    rnd_overlap, _, _ = overlap_graph(cc)
+    print(f"Number of overlap edges in random data: {rnd_overlap.number_of_edges()} %: {rnd_overlap.number_of_edges() / obs_overlap}")
+    return rnd_encap.number_of_edges(), rnd_overlap_dag.number_of_edges(), rnd_overlap.number_of_edges()
+
+
+
+filename = "../data/coauth-MAG-History/coauth-MAG-History-"
+random_path = "../data/coauth-MAG-History/randomizations/"
+num_samples = 10
+
+print("Reading hyperedges...")
+hyperedges = read_data(filename)
+#hyperedges = read_hyperedges(filename)
+print("Done.")
+
+print("Computing largest connected component...")
+cc = largest_connected_component(hyperedges, remove_single_nodes=True)
+print("Done.")
+
+obs_encap, obs_overdag, obs_overlap = print_observed_stats(cc)
+print()
+
+for _ in range(num_samples):
+    print("Reading configuration model data...")
+    random = read_hyperedges(random_path + f"random-simple-{_}.txt")
+    cc = largest_connected_component(random, remove_single_nodes=True)
+    print_random_stats(cc, obs_encap, obs_overdag, obs_overlap)
+    print()
+
+    print("Computing layer randomization...")
+    random = layer_randomization(hyperedges)
+    cc = largest_connected_component(random, remove_single_nodes=True)
+    print_random_stats(cc, obs_encap, obs_overdag, obs_overlap)
+    print()
