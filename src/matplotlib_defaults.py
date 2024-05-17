6ce2b1cf3372b133be5bf7d4ce55cab63f495f4f
Timothy LaRock <timothylarock@gmail.com>
2024-05-17 09:18:11 +0100

    Added matplotlib_defaults convenience file.

---
+++ src/matplotlib_defaults.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.size'] = 14
+plt.rcParams['axes.labelsize'] = 13
+plt.rcParams['legend.fontsize'] = 12
+plt.rcParams['ytick.labelsize'] = 11
+plt.rcParams['xtick.labelsize'] = 11
